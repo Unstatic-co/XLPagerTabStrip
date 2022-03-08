9f8b015e4ec779c36d0d8becc4087ec1fda03456
jean <jean@witick.io>
2022-03-08 15:19:14 +0100

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.5.0
+import PackageDescription
+
+let package = Package(
+    name: "XLPagerTabStrip",
+    products: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"]),
+    ],
+    targets: [
+        .target(
+            name: "XLPagerTabStrip",
+	    path: "Sources",
+	    exclude: [
+	    	"FXPageControl.h",
+     	    	"FXPageControl.m"
+	    ]
+    ]
+)
